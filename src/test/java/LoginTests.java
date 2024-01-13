c95cb7ce8691c385601103ba4ed1ff6f636984c8
Ekaterina Ponomareva <katyushkaashihmina@yandex.ru>
2024-01-13 17:52:30 +0500

    fifth_commit

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,5 +1,7 @@
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,13 +12,29 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
-public class LoginTests {
+import static org.apache.http.HttpStatus.SC_ACCEPTED;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.junit.Assert.assertEquals;
 
+public class LoginTests {
     private WebDriver webDriver;
     private LoginPage loginPage;
+    private String email;
+    private String password;
+    private String name;
+    private static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+    private String fullToken;
+    private User user;
+    private UserClient userClient;
+    private Response response;
 
     @Before
     public void setUp() {
+        RestAssured.baseURI = BASE_URL;
+        user = UserGenerator.randomUser();
+        userClient = new UserClient();
+        response = userClient.create(user);
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         webDriver = new ChromeDriver(options);
@@ -27,10 +45,15 @@ public class LoginTests {
     @DisplayName("Логин пользователя через кнопку войти в аккаунт")
     @Description("Успешный логин пользователя через кнопку войти в аккаунт")
     public void loginThrowEnterAcoountBottomTest() {
+        fullToken = response.path("accessToken");
+        email= user.getEmail();
+        password = user.getPassword();
+        name = user.getName();
+        assertEquals("Неверный статус код", SC_OK, response.statusCode());
         loginPage = new LoginPage(webDriver);
         webDriver.get("https://stellarburgers.nomoreparties.site");
         loginPage.goToLoginPage();
-        loginPage.LoggedIn("annaponomareva@mail.ru", "123456");
+        loginPage.LoggedIn(email, password);
         Assert.assertEquals(true, loginPage.isMainPageTitleIsVisible());
     }
 
@@ -38,10 +61,14 @@ public class LoginTests {
     @DisplayName("Логин пользователя через кнопку личного кабинета")
     @Description("Успешный логин пользователя через кнопку личного кабинета")
     public void loginThrowEnterPersonalAcoountBottomTest() {
+        fullToken = response.path("accessToken");
+        email= user.getEmail();
+        password = user.getPassword();
+        name = user.getName();
         loginPage = new LoginPage(webDriver);
         webDriver.get("https://stellarburgers.nomoreparties.site");
         loginPage.goToPersonalAccount();
-        loginPage.LoggedIn("annaponomareva@mail.ru", "123456");
+        loginPage.LoggedIn(email, password);
         Assert.assertEquals(true, loginPage.isMainPageTitleIsVisible());
     }
 
@@ -49,12 +76,16 @@ public class LoginTests {
     @DisplayName("Логин пользователя через кнопку в форме регистрации")
     @Description("Успешный логин пользователя через кнопку в форме регистрации")
     public void loginThrowEnterRegistrBottomTest() {
+        fullToken = response.path("accessToken");
+        email= user.getEmail();
+        password = user.getPassword();
+        name = user.getName();
         loginPage = new LoginPage(webDriver);
         webDriver.get("https://stellarburgers.nomoreparties.site");
         loginPage.goToPersonalAccount();
         loginPage.goToRegistrPage();
         loginPage.goToLoginPageFromRegistrRestorePasswordPage();
-        loginPage.LoggedIn("annaponomareva@mail.ru", "123456");
+        loginPage.LoggedIn(email, password);
         Assert.assertEquals(true, loginPage.isMainPageTitleIsVisible());
     }
 
@@ -62,17 +93,24 @@ public class LoginTests {
     @DisplayName("Логин пользователя через кнопку в форме восстановления пароля")
     @Description("Успешный логин пользователя через кнопку в форме восстановления пароля")
     public void loginThrowEnterRestorePasswordBottomTest() {
+        fullToken = response.path("accessToken");
+        email= user.getEmail();
+        password = user.getPassword();
+        name = user.getName();
         loginPage = new LoginPage(webDriver);
         webDriver.get("https://stellarburgers.nomoreparties.site");
         loginPage.goToPersonalAccount();
         loginPage.goToRestorePasswordPage();
         loginPage.goToLoginPageFromRegistrRestorePasswordPage();
-        loginPage.LoggedIn("annaponomareva@mail.ru", "123456");
+        loginPage.LoggedIn(email, password);
         Assert.assertEquals(true, loginPage.isMainPageTitleIsVisible());
     }
 
     @After
     public void teardown() {
+        UserClient userClient = new UserClient();
+        Response delete = userClient.delete(fullToken);
+        assertEquals("Неверный статус код", SC_ACCEPTED, delete.statusCode());
         webDriver.quit();
     }
 }
